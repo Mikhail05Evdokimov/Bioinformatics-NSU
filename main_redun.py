fde82587dab977bf580f9a83907750a01448101f
ubuntu <evdokimm05@gmail.com>
2024-12-04 16:14:27 +0700

    dag file

--- main_redun.py
+++ main_redun.py
@@ -5,17 +5,19 @@ from redun import task, File
 def bwa(fastq, reference):
     subprocess.run(f"./bwa/bwa/bwa index {reference}", shell=True)
     subprocess.run(f"./bwa/bwa/bwa mem {reference} {fastq} | gzip -3 > aln-se.sam.gz", shell=True)
+    return File("aln-se.sam.gz")
 
 
 @task()
-def samtools_view():
-    subprocess.run(f"./samtools-1.21/samtools view -Sb aln-se.sam.gz > aln-se.bam", shell=True)
+def samtools_view(bwa_file):
+    subprocess.run(f"./samtools-1.21/samtools view -Sb {bwa_file.path} > aln-se.bam", shell=True)
     subprocess.run(f"./samtools-1.21/samtools sort aln-se.bam -o aln-se.sorted.bam", shell=True)
+    return File("aln-se.sorted.bam")
 
 
 @task()
-def samtools_flagstat():
-    subprocess.run(f"./samtools-1.21/samtools flagstat aln-se.sorted.bam > flagstat_report.txt", shell=True)
+def samtools_flagstat(view_file):
+    subprocess.run(f"./samtools-1.21/samtools flagstat {view_file.path} > flagstat_report.txt", shell=True)
     return File("flagstat_report.txt")
 
 
@@ -25,35 +27,35 @@ def parse_percent(report):
         lines = file.readlines()
         for line in lines:
             try:
-		ind_start = line.index("mapped (") + 8
-		ind_end = line[ind_start:].index
+                ind_start = line.index("mapped (") + 8
+                ind_end = line[ind_start:].index("%") + ind_start
                 with open("percent.txt", 'w+') as percent_file:
                     percent_file.write(line[ind_start:ind_end])
-		break
-	    except Exception :
-		pass
+                break
+            except Exception as e :
+                print(e)
 
     return File("percent.txt")
 
 @task()
-def get_results():
-    with open("percent_txt", 'r') as f:
+def get_results(percent_file):
+    with open(percent_file.path, 'r') as f:
         percent_value = float(f.read())
 
     if percent_value < 90:
         print("not OK")
-	return "NO"
+        return "NO"
     else:
         print("OK")
-	return "OK"
+        return "OK"
 
 @task()
 def main():
     fastqPath = "SRR31254081.fastq"
     referencePath = "GCA.fna"
-    bwa(fastqPath, referencePath)
-    samtools_view()
-    report = samtools_flagstat()
-    parse_percent(report)
-    res = get_results()
+    bwa_file = bwa(fastqPath, referencePath)
+    view_file = samtools_view(bwa_file)
+    report = samtools_flagstat(view_file)
+    percent_file = parse_percent(report)
+    res = get_results(percent_file)
     return res
